ea8e7cff2a1fff2103549608f07afe76c102e994
Nizvan Monteón Ricárdez <njr01397@gmail.com>
2023-04-04 22:30:08 -0600

    Elaboraciónde del tema habilidades cognoscitivas v0.1

--- src/public/assets/js/fuentes.js
+++ src/public/assets/js/fuentes.js
@@ -1,4 +1,8 @@
 function cambiarColor(id, color) {
     let fuente = document.getElementById(id);
     fuente.classList.add(color);
+	setTimeout(() => {
+		fuente.classList.remove(color)
+		fuente.classList.add('color-blanco')
+	}, 5000);
 }
