8440f7cb4cd23563df9f29cedf1dc9349e1a663c
chocovivis <19161403@itoaxaca.edu.mx>
2023-04-12 23:55:53 -0600

    correcciones estéticas y avance de temas unidad 4

--- src/controllers/temasU4.controller.ts
+++ src/controllers/temasU4.controller.ts
@@ -7,3 +7,10 @@ export function robotica(req:Request, res:Response){
 export function clasificacion(req:Request, res:Response){
     res.render('unidad4/clasificacion');
 }
+export function desarrollos_y_aplicaciones(req:Request, res:Response){
+    res.render('unidad4/desarrollos_y_aplicaciones');
+}
+
+export function redes_neuronales(req:Request, res:Response){
+    res.render('unidad4/redes_neuronales');
+}
