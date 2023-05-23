9a4fe0bd37e559e1db2510dbf690316c2d1eb05c
JafedCalvo <19161246@itoaxaca.edu.mx>
2023-05-23 00:21:35 -0600

    Unidad 3 del temario

--- src/routes/temasU3.router.ts
+++ src/routes/temasU3.router.ts
@@ -1,8 +1,13 @@
 import { Router } from 'express';
-import { representacion_de_conocimientos } from '../controllers/temasU3.controller';
+import { representacion_de_conocimientos, metodos_de_inferencia, reglas_de_produccion, sintaxis_de_las_reglas, semantica_reglas, arquitectura_sistema } from '../controllers/temasU3.controller';
 
 const router:Router = Router();
 
 router.get('/representacion_conocimientos', representacion_de_conocimientos);
+router.get('/metodos_de_inferencia', metodos_de_inferencia);
+router.get('/reglas_de_produccion', reglas_de_produccion);
+router.get('/sintaxis_de_las_reglas', sintaxis_de_las_reglas );
+router.get('/semantica_reglas', semantica_reglas);
+router.get('/arquitectura_sistema', arquitectura_sistema);
 
 export default router;
