5a2d0cfa6ba4fa0dc2fd52a82eb9e75524b38cfa
Nizvan MR <njr01397@gmail.com>
2023-05-26 14:55:25 -0600

    Agregando la vista de Bibliografía

--- src/controllers/main.controller.ts
+++ src/controllers/main.controller.ts
@@ -3,3 +3,7 @@ import { Request, Response } from 'express';
 export function main(req:Request, res:Response){
     res.render('main/index.ejs');
 }
+
+export function bibliografia(req:Request, res:Response){
+    res.render('bibliografia.ejs');
+}
