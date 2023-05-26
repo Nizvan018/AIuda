61659423720e7668c4ae303b1b10d65612ed9d11
Oliver <19161255@itoaxaca.edu.mx>
2023-05-25 18:06:01 -0600

    Temas de la unidad 2

--- src/controllers/temasU2.controller.ts
+++ src/controllers/temasU2.controller.ts
@@ -3,3 +3,31 @@ import { Request, Response } from 'express';
 export function principios_y_metodologia(req:Request, res:Response){
     res.render('unidad2/principios_metodologiaIA.ejs');
 }
+
+export function paradigmas(req:Request, res:Response){
+    res.render('unidad2/paradigmas_IA.ejs');
+}
+
+export function mapas_conceptuales(req:Request, res:Response){
+    res.render('unidad2/mapas_conceptuales.ejs');
+}
+export function redes_semanticas(req:Request, res:Response){
+    res.render('unidad2/redes_semanticas.ejs');
+}
+
+export function razonamiento_monotono(req:Request, res:Response){
+    res.render('unidad2/razonamiento_monotono.ejs');
+}
+
+export function otras_logicas(req:Request, res:Response){
+    res.render('unidad2/otras_logicas.ejs');
+}
+export function razonamiento_probabilistico(req:Request, res:Response){
+    res.render('unidad2/razonamiento_probabilistico.ejs');
+}
+export function teorema_de_Bayes(req:Request, res:Response){
+    res.render('unidad2/teorema_de_Bayes.ejs');
+}
+// export function (req:Request, res:Response){
+//     res.render('unidad2/.ejs');
+// }
--- src/routes/temasU2.router.ts
+++ src/routes/temasU2.router.ts
@@ -1,8 +1,16 @@
 import { Router } from 'express';
-import { principios_y_metodologia } from '../controllers/temasU2.controller';
+import { principios_y_metodologia, paradigmas, mapas_conceptuales, redes_semanticas, razonamiento_monotono, otras_logicas, razonamiento_probabilistico,teorema_de_Bayes } from '../controllers/temasU2.controller';
 
-const router:Router = Router();
+const router: Router = Router();
 
 router.get('/principios_y_metodologia', principios_y_metodologia);
+router.get('/paradigmas', paradigmas);
+router.get('/mapas_conceptuales', mapas_conceptuales);
+router.get('/redes_semanticas', redes_semanticas);
+
+router.get('/razonamiento_monotono', razonamiento_monotono);
+router.get('/otras_logicas', otras_logicas);
+router.get('/razonamiento_probabilistico',razonamiento_probabilistico);
+router.get('/teorema_de_Bayes',teorema_de_Bayes);
 
 export default router;
