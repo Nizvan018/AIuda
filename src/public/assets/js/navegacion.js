f4cb5128676776113c156fd79d8e050c2a12b514
chocovivis <19161403@itoaxaca.edu.mx>
2023-05-27 00:32:50 -0600

    Cambios en la barra de navegación

---
+++ src/public/assets/js/navegacion.js
@@ -0,0 +1,29 @@
+document.getElementById('robotica1').addEventListener('click', function() {
+    window.location.href = '/aplicaciones_IA/robotica/conceptos_basicos';
+  });
+document.getElementById('rn1').addEventListener('click', function() {
+    window.location.href = '/aplicaciones_IA/redes_neuronales/conceptos_basicos';
+  });
+document.getElementById('va1').addEventListener('click', function() {
+    window.location.href = '/aplicaciones_IA/vision_artificial/conceptos_basicos';
+  });
+document.getElementById('ld1').addEventListener('click', function() {
+    window.location.href = '/aplicaciones_IA/logica_difusa/conceptos_basicos';
+  });
+
+document.getElementById('pln1').addEventListener('click', function() {
+    window.location.href = '/aplicaciones_IA/procesamiento_del_lenguje_natural/conceptos_basicos';
+  });
+  document.getElementById('se1').addEventListener('click', function() {
+    window.location.href = '/aplicaciones_IA/sistemas_expertos/conceptos_basicos';
+  });
+
+  document.getElementById('u1').addEventListener('click', function() {
+    window.location.href = '/inteligencia_artificial/definicion_historia';
+  });
+  document.getElementById('u2').addEventListener('click', function() {
+    window.location.href = '/aspectos_metodologicos/principios_y_metodologia';
+  });
+  document.getElementById('u3').addEventListener('click', function() {
+    window.location.href = '/reglas_busquedas/representacion_conocimientos';
+  });
