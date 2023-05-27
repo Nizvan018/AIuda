6f225fb3dfc8567ad7b662001c911210928db48c
Oliver <19161255@itoaxaca.edu.mx>
2023-05-27 00:30:21 -0600

    About Us

--- src/routes/main.router.ts
+++ src/routes/main.router.ts
@@ -1,9 +1,10 @@
 import { Router } from 'express';
-import { bibliografia, main } from '../controllers/main.controller';
+import { bibliografia, aboutus, main } from '../controllers/main.controller';
 
 const router:Router = Router();
 
 router.get('/', main);
 router.get('/bibliografia', bibliografia);
+router.get('/about-us', aboutus);
 
 export default router;
