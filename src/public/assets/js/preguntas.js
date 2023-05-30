858f835ffeca09b9e112abb55f0b38aa0336ab2b
Nizvan MR <njr01397@gmail.com>
2023-05-29 19:28:59 -0600

    Mejorando funcionalidad de las preguntas para la Unidad 1

--- src/public/assets/js/preguntas.js
+++ src/public/assets/js/preguntas.js
@@ -1,6 +1,20 @@
 function evaluar(event, color){
-    let elemento = event.target;
+    let $respuesta = event.target;
+    let $mensaje = $respuesta.parentElement.getElementsByClassName('mensaje')[0];
+    let $respuestas = Array.from($respuesta.parentElement.getElementsByClassName('respuesta'));
 
-    elemento.classList.add(color);
-    elemento.style.border = "none";
+    $respuesta.classList.add(color);
+    $respuesta.style.border = 'none';
+
+    if(color === 'fondo-verde'){
+        $mensaje.innerHTML = '¡La respuesta es correcta, bien hecho!';
+        $mensaje.classList.add('color-verde');
+        $mensaje.classList.remove('color-rojo');
+        
+        $respuestas.map(res => res.removeAttribute('onclick'));
+    } else{
+        $mensaje.innerHTML = 'Respuesta incorrecta :C, ¡inténtalo de nuevo!';
+        $mensaje.classList.add('color-rojo');
+        $mensaje.classList.remove('color-verde');
+    }
 }
