517375f9cd971182a3de33d1e05f9e133d4cd7c6
Nizvan MR <njr01397@gmail.com>
2023-05-30 16:36:23 -0600

    Arreglos finales para su despliegue

--- src/controllers/temas.controller.ts
+++ src/controllers/temas.controller.ts
@@ -3,12 +3,3 @@ import { Request, Response } from 'express';
 export function inteligencia_artificial(req:Request, res:Response){
     res.render('tema_inteligencia_artificial/definicion_historia.ejs');
 }
-export function inteligencia_artificial(req:Request, res:Response){
-    res.render('tema_inteligencia_artificial/definicion_historia.ejs');
-}
-export function inteligencia_artificial(req:Request, res:Response){
-    res.render('tema_inteligencia_artificial/definicion_historia.ejs');
-}
-export function inteligencia_artificial(req:Request, res:Response){
-    res.render('tema_inteligencia_artificial/definicion_historia.ejs');
-}
